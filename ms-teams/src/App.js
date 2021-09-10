227bdf3eb111091211ba18ac7f49b8f67209d6ea
Pawan Kumar Dubey <pawan.dubey@vodafone.com>
2021-09-10 12:44:38 +0530

    second commit

---
+++ ms-teams/src/App.js
@@ -0,0 +1,16 @@
+import { ChatEngine } from 'react-chat-engine';
+import './App.css'
+import React from 'react'
+
+const App = () => {
+    return (
+        <ChatEngine
+             height = "100vh"
+             projectID = "d0903f00-f999-409d-b9a2-55107cc1c7ee"
+             userName = "pawan"
+             userSecret = "123123"
+             />
+    )
+}
+
+export default App
